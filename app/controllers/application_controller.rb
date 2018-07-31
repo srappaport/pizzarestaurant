005e8ccfeffe39455ea403e36ff58ec1e3519681
srappaport <sarah.rappaport@greendot.org>
2018-07-31 01:39:48 +0000

    Made pizza website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+
+  get '/order_pizza' do
+    erb :order_pizza
+  end
+
+  post '/pricing' do
+    #puts params[:cheese]
+    puts params
+    @cheese = params[:cheese]
+    @sauce = params[:sauce]
+    @toppings = params[:topping]
+    @total = 10
+    @total=cheese_price(@cheese, @total)
+    @total=sauce_price(@sauce, @total)
+    @total=topping_price(@toppings, @total)
+
+    erb :pricing
+  end
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,42 @@
-class SampleModel
-  attr_accessor :name, :age
+def cheese_price(cheese, total)
+  if cheese.downcase == "mozzarella"
+    "The price is $1.00"
+    total+=1
+  elsif cheese.downcase == "goat cheese"
+    "The price is $1.50"
+    total+=1.5
+  else
+    "No additional charge"
+  end
+  total
+end
+
+def sauce_price(sauce, total)
+  if sauce.downcase == "tomato"
+    "The price is $0.50"
+    total+=0.50
+  elsif sauce.downcase == "pesto"
+    "The price is $0.75"
+    total+= 0.75
+  else
+    "No additional charge"
+  end
+  total
+end
+
+def topping_price(topping, total)
+  topping.each do |topping|
+  if topping.downcase == "olives"
+    "The price is $0.25"
+    total+=0.25
+  elsif topping.downcase == "mushrooms"
+    "The price is $0.35"
+    total+= 0.35
+  elsif topping.downcase == "onions"
+    total+= 0.40
+  else
+    "No additional charge"
+  end
+end
+  total
 end
